2f6c027517969ca606de2ad15e32f808a74dbef0
renyue <1005833756@qq.com>
2018-08-18 11:48:59 +0800

    替换为spring2.0.4

--- src/main/java/com/jesper/seckill/MainApplication.java
+++ src/main/java/com/jesper/seckill/MainApplication.java
@@ -3,6 +3,8 @@ package com.jesper.seckill;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.Duration;
+
 @SpringBootApplication
 public class MainApplication{
 	public static void main(String[] args) {
